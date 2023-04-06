43bf069f62f5998e228417c2c99e2d9caad52885
alexandruiradu <alexandruradu4288@outlook.com>
2023-04-06 20:01:13 +0300

    [BMS-8] - As a user, I should be able to delete an author from the list of authors existing in the system

--- src/main/java/org/sda/bms/service/AuthorServiceImpl.java
+++ src/main/java/org/sda/bms/service/AuthorServiceImpl.java
@@ -3,7 +3,9 @@ package org.sda.bms.service;
 import org.sda.bms.model.Author;
 import org.sda.bms.repository.AuthorRepository;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 public class AuthorServiceImpl implements AuthorService {
     private static final String NAME_VALIDATION_REGEX = "^[a-zA-Z]+$";
@@ -21,21 +23,25 @@ public class AuthorServiceImpl implements AuthorService {
                     "Provided first name is empty or blank. Provide a valid value."
             );
         }
+
         if (lastName == null || lastName.isBlank() || lastName.isEmpty()) {
             throw new IllegalArgumentException(
                     "Provided last name is empty or blank. Provide a valid value."
             );
         }
+
         if (!firstName.matches(NAME_VALIDATION_REGEX)) {
             throw new IllegalArgumentException(
                     "Provided first name contains invalid characters. Provide a valid value."
             );
         }
+
         if (!lastName.matches(NAME_VALIDATION_REGEX)) {
             throw new IllegalArgumentException(
                     "Provided last name contains invalid characters. Provide a valid value."
             );
         }
+
         Author author = new Author(firstName, lastName);
         authorRepository.create(author);
     }
@@ -43,5 +49,23 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public List<Author> findAll() {
         return authorRepository.findAll();
+    }
+
+    @Override
+    public void delete(int id) {
+        if (id <= 0) {
+            throw new IllegalArgumentException(
+                    "Provided id is negative or 0. Provide a valid value."
+            );
+        }
+
+        Optional<Author> authorOptional = authorRepository.findById(id);
+        if (authorOptional.isEmpty()) {
+            throw new EntityNotFoundException(
+                    "Author with provided id was not found in the system."
+            );
+        }
+
+        authorRepository.delete(authorOptional.get());
     }
 }
