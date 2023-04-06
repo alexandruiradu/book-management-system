5b76c70ce73578b125d4e8a67b5322bbb8240603
alexandruiradu <alexandruradu4288@outlook.com>
2023-04-06 20:30:29 +0300

    [BMS-9] - As a user, I should be able to update an existing author

--- src/main/java/org/sda/bms/controller/AuthorController.java
+++ src/main/java/org/sda/bms/controller/AuthorController.java
@@ -4,6 +4,7 @@ import org.sda.bms.model.Author;
 import org.sda.bms.repository.exception.EntityCreationFailedException;
 import org.sda.bms.repository.exception.EntityDeletionFailedException;
 import org.sda.bms.repository.exception.EntityFetchingFailedException;
+import org.sda.bms.repository.exception.EntityUpdateFailedException;
 import org.sda.bms.service.AuthorService;
 
 import javax.persistence.EntityNotFoundException;
@@ -71,6 +72,32 @@ public class AuthorController {
         } catch (EntityFetchingFailedException e) {
             System.err.println(e.getMessage());
         } catch (EntityDeletionFailedException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            System.err.println("Internal server error. Please contact your administrator.");
+        }
+    }
+
+    public void updateById() {
+        try{
+            System.out.println("Please provide the author id:");
+            int authorId = Integer.parseInt(scanner.nextLine().trim());
+            System.out.println("Please provide author first name:");
+            String firstName = scanner.nextLine().trim();
+            System.out.println("Please provide author last name:");
+            String lastName = scanner.nextLine().trim();
+
+            authorService.update(authorId, firstName, lastName);
+            System.out.println("Author was updated successfully");
+        }catch (NumberFormatException e) {
+            System.err.println("Provided id is not a number. Provide a valid value.");
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        } catch (EntityNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (EntityFetchingFailedException e) {
+            System.err.println(e.getMessage());
+        } catch (EntityUpdateFailedException e) {
             System.err.println(e.getMessage());
         } catch (Exception e) {
             System.err.println("Internal server error. Please contact your administrator.");
--- src/main/java/org/sda/bms/service/AuthorService.java
+++ src/main/java/org/sda/bms/service/AuthorService.java
@@ -9,4 +9,5 @@ public interface AuthorService {
     void create(String firstName, String lastName);
     List<Author> findAll();
     void delete(int id);
+    void update(int id, String firstName, String lastName);
 }
